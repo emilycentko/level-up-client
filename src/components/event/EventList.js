255a2a22650d42a78a7e6bd5618b47f7f8f04df1
emilycentko <emily.centko@gmail.com>
2021-05-07 11:20:01 -0500

    Added createEvent function and form to create a new event

--- src/components/event/EventList.js
+++ src/components/event/EventList.js
@@ -1,38 +1,49 @@
 import React, { useContext, useEffect } from "react"
 import { EventContext } from "./EventProvider.js"
+import { useHistory } from "react-router-dom"
 
 export const EventList = (props) => {
     const { events, getEvents } = useContext(EventContext)
+
+    const history = useHistory()
 
     useEffect(() => {
         getEvents()
     }, [])
 
     return (
-        <article className="events">
-            <header className="events__header">
-                <h1>Level Up Game Events</h1>
-            </header>
-            {
-                events.map(event => {
-                    return <section key={event.id} className="registration">
-                        <h3 className="registration__game">{event.name}</h3>
-                        <div>{event.game.name}</div>
-                        <div>
-                            {
-                                new Date(event.date).toLocaleDateString("en-US",
+        
+        <>
+            <button className="btn btn-2 icon-create"
+                onClick={() => {
+                    history.push({ pathname: "/events/new" })
+                }}
+                >Schedule New Event</button>
+            <article className="events">
+                <header className="events__header">
+                    <h1>Level Up Game Events</h1>
+                </header>
+                {
+                    events.map(event => {
+                        return <section key={event.id} className="registration">
+                            <h3 className="registration__game">{event.name}</h3>
+                            <div>{event.game.name}</div>
+                            <div>
                                 {
-                                    weekday: 'long',
-                                    year: 'numeric',
-                                    month: 'long',
-                                    day: 'numeric'
-                                })
-                            }
-                             : {event.time} pm
-                        </div>
-                    </section>
-                })
-            }
-        </article >
+                                    new Date(event.date).toLocaleDateString("en-US",
+                                    {
+                                        weekday: 'long',
+                                        year: 'numeric',
+                                        month: 'long',
+                                        day: 'numeric'
+                                    })
+                                }
+                                : {event.time} pm
+                            </div>
+                        </section>
+                    })
+                }
+            </article >
+        </>
     )
 }
